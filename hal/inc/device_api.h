bca608f0675ed48a04d4851903df08b874b9b534
Moritz Scherer <scheremo@iis.ee.ethz.ch>
2024-08-06 19:09:36 +0200

    Basic SDK setup

---
+++ hal/inc/device_api.h
@@ -0,0 +1,26 @@
+// Copyright 2024 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Moritz Scherer <scheremo@iis.ee.ethz.ch>
+
+#include <stdio.h>
+#include <stdint.h>
+
+typedef bool (*chi_device_callback)(struct chi_device *device);
+
+typedef struct chi_device_api
+{
+  int (*open)(struct chi_device *device);
+  int (*close)(struct chi_device *device);
+  ssize_t (*read_async)(struct chi_device *device,
+                        void *buffer, uint32_t size, chi_device_callback cb);
+  ssize_t (*write_async)(struct chi_device *device,
+                         const void *buffer, uint32_t size, chi_device_callback cb);
+} chi_device_api_t;
+
+typedef struct chi_device {
+  struct chi_device_api *api; // function pointers
+  uint32_t* device_addr;
+  void *cfg;
+} chi_device_t;
---
+++ hal/src/device_api.c
@@ -0,0 +1,8 @@
+// Copyright 2024 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Moritz Scherer <scheremo@iis.ee.ethz.ch>
+
+#include <stdio.h>
+#include <stdint.h>
