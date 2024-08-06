bf346579ea900b53c516ea978bd66ec526935745
Moritz Scherer <scheremo@iis.ee.ethz.ch>
2024-08-06 21:36:12 +0200

    Add basic linting CI
    Touchup test-gen output directory

--- hal/inc/device_api.h
+++ hal/inc/device_api.h
@@ -10,16 +10,16 @@
 typedef bool (*chi_device_callback)(struct chi_device *device);
 
 typedef struct chi_device_api {
-  int (*open)(struct chi_device *device);
-  int (*close)(struct chi_device *device);
-  ssize_t (*read_async)(struct chi_device *device, void *buffer, uint32_t size,
-                        chi_device_callback cb);
-  ssize_t (*write_async)(struct chi_device *device, const void *buffer,
-                         uint32_t size, chi_device_callback cb);
+    int (*open)(struct chi_device *device);
+    int (*close)(struct chi_device *device);
+    ssize_t (*read_async)(struct chi_device *device, void *buffer, uint32_t size,
+                          chi_device_callback cb);
+    ssize_t (*write_async)(struct chi_device *device, const void *buffer, uint32_t size,
+                           chi_device_callback cb);
 } chi_device_api_t;
 
 typedef struct chi_device {
-  struct chi_device_api *api; // function pointers
-  uint32_t *device_addr;
-  void *cfg;
+    struct chi_device_api *api; // function pointers
+    uint32_t *device_addr;
+    void *cfg;
 } chi_device_t;
