c07f536d416ac3dc8791520f1c52f8160dd596c6
viv-eth <vivianep@iis.ee.ethz.ch>
2024-11-01 16:17:05 +0100

    [drivers] Sparse checkout of OpenTitan sources

--- cmake/opentitan.cmake
+++ cmake/opentitan.cmake
@@ -8,11 +8,34 @@ include(ExternalProject)
 
 set(OPENTITAN_DIR ${CMAKE_CURRENT_BINARY_DIR}/../drivers/opentitan)
 
+if(EXISTS ${OPENTITAN_DIR})
+  file(REMOVE_RECURSE ${OPENTITAN_DIR})
+endif()
+
+set(SPARSE_CHECKOUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/sparse-checkout)
+
+set(SPARSE_PATTERNS
+  "util/*"
+  "hw/ip/*"
+  "sw/device/lib/base/*"
+  "sw/device/lib/base/internal/*"
+  "sw/device/lib/dif/*"
+  "sw/device/lib/dif/autogen/*"
+)
+
+file(WRITE ${SPARSE_CHECKOUT_FILE} "")
+foreach(PATTERN ${SPARSE_PATTERNS})
+  file(APPEND ${SPARSE_CHECKOUT_FILE} "${PATTERN}\n")
+endforeach()
+
 # Add OpenTitan as an external project
 ExternalProject_Add(
   opentitan
-  GIT_REPOSITORY https://github.com/lowRISC/opentitan.git
-  GIT_TAG master  # Consider specifying a specific tag or commit for stability
+  DOWNLOAD_COMMAND
+    git clone --no-checkout https://github.com/lowRISC/opentitan.git ${OPENTITAN_DIR}
+    COMMAND git -C ${OPENTITAN_DIR} config core.sparseCheckout true
+    COMMAND ${CMAKE_COMMAND} -E copy ${SPARSE_CHECKOUT_FILE} ${OPENTITAN_DIR}/.git/info/sparse-checkout
+    COMMAND git -C ${OPENTITAN_DIR} checkout master
   UPDATE_COMMAND ""  # Disable updates to avoid unexpected changes
   CONFIGURE_COMMAND ""  # No configure step needed
   BUILD_COMMAND ""  # No build step needed
