986b41c4f682b6a394c43d8c73e6301b4904b902
Philip Wiese <wiesep@student.ethz.ch>
2024-11-29 01:43:16 +0100

    [target] Add CONVOLVE cluster target

---
+++ targets/chimera-convolve/include/cluster_4.h
@@ -0,0 +1,13 @@
+// Copyright 2024 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Philip Wiese <wiesep@iis.ee.ethz.ch>
+
+#ifndef _CLUSTER_4_INCLUDE_GUARD_
+#define _CLUSTER_4_INCLUDE_GUARD_
+
+#include "regs/cluster_4_reg.h"
+#include "addr_maps/cluster_4_addr_map.h"
+
+#endif // _CLUSTER_4_INCLUDE_GUARD_
