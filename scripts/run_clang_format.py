ba5a16bb0f419afe6b6a622f20ca0d05ef4828ba
Victor Jung <jungvi@iis.ee.ethz.ch>
2025-01-16 17:15:09 +0100

    Fix gitlab-ci and linting

--- scripts/run_clang_format.py
+++ scripts/run_clang_format.py
@@ -124,14 +124,12 @@ def make_diff(file, original, reformatted):
 
 
 class DiffError(Exception):
-
     def __init__(self, message, errs=None):
         super(DiffError, self).__init__(message)
         self.errs = errs or []
 
 
 class UnexpectedError(Exception):
-
     def __init__(self, message, exc=None):
         super(UnexpectedError, self).__init__(message)
         self.formatted_traceback = traceback.format_exc()
@@ -227,7 +225,6 @@ def bold_red(s):
 
 
 def colorize(diff_lines):
-
     def bold(s):
         return '\x1b[1m' + s + '\x1b[0m'
 
