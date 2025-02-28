1f0018c212735c666a935e06500fb8d592d1dd26
Sergio Mazzola <smazzola@iis.ee.ethz.ch>
2025-02-28 15:29:12 +0100

    target: Implement chimera-pulp target

--- targets/chimera-pulp/include/addr_maps/soc_addr_map.h
+++ targets/chimera-pulp/include/addr_maps/soc_addr_map.h
@@ -4,5 +4,54 @@
 //
 // Sergio Mazzola <smazzola@iis.ee.ethz.ch>
 
-#error "Not implemented"
-//TODO(smazzola): Implement
+#ifndef _SOC_ADDR_MAP_INCLUDE_GUARD_
+#define _SOC_ADDR_MAP_INCLUDE_GUARD_
+
+#include <stdint.h>
+
+//////////
+// Host //
+//////////
+
+#define CLINT_CTRL_BASE 0x02040000
+
+////////////////
+// SoC regbus //
+////////////////
+
+#define SOC_CTRL_BASE                 0x30001000
+#define CHIMERA_PADFRAME_BASE_ADDRESS 0x30002000
+#define FLL_BASE_ADDR                 0x30003000
+#define HYPERBUS_CFG_BASE             0x30005000
+
+//////////////////
+// SoC AXI Xbar //
+//////////////////
+
+#define CLUSTER_0_BASE 0x40000000
+#define CLUSTER_1_BASE 0x40400000
+#define CLUSTER_2_BASE 0x40800000
+#define CLUSTER_3_BASE 0x40C00000
+#define CLUSTER_4_BASE 0x41000000
+
+#define HYPERRAM_BASE  0x50000000
+
+///////////////
+// Hw params //
+///////////////
+
+#define CLUSTER_0_NUMCORES 8
+#define CLUSTER_1_NUMCORES 8
+#define CLUSTER_2_NUMCORES 8
+#define CLUSTER_3_NUMCORES 8
+#define CLUSTER_4_NUMCORES 8
+
+#define NUM_CLUSTER_CORES \
+    (CLUSTER_0_NUMCORES + CLUSTER_1_NUMCORES + CLUSTER_2_NUMCORES + CLUSTER_3_NUMCORES + \
+     CLUSTER_4_NUMCORES)
+
+static uint8_t _chimera_numCores[] = {CLUSTER_0_NUMCORES, CLUSTER_1_NUMCORES, CLUSTER_2_NUMCORES,
+                                      CLUSTER_3_NUMCORES, CLUSTER_4_NUMCORES};
+#define _chimera_numClusters 5
+
+#endif
