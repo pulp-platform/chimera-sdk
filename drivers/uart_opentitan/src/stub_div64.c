f69f8eed9d3de7b8d9fc6ef4108f3e66bb2635e9
viv-eth <vivianep@iis.ee.ethz.ch>
2025-03-19 14:47:47 +0100

    [drivers] Integrate Python wrapper in cmake build for OT periphs

--- drivers/uart_opentitan/src/stub_div64.c
+++ drivers/uart_opentitan/src/stub_div64.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
-__attribute__((weak, warning("64-bit arithmetic operation detected; ensure you're using the intended alternative implementation.")))
-void _ot_builtin_div64_intentionally_not_implemented_see_pull_11451(void) {
+__attribute__((weak, warning("64-bit arithmetic operation detected; ensure you're using the "
+                             "intended alternative implementation."))) void
+_ot_builtin_div64_intentionally_not_implemented_see_pull_11451(void) {
     // Abort execution if this function is ever actually called.
     abort();
 }
