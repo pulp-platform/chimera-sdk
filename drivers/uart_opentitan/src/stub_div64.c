393dc3329fd88a416f9272377594a0c3ff16099d
viv-eth <vivianep@iis.ee.ethz.ch>
2025-03-31 15:43:08 +0200

    [lint] Add licensing and proper formatting

--- drivers/uart_opentitan/src/stub_div64.c
+++ drivers/uart_opentitan/src/stub_div64.c
@@ -1,3 +1,9 @@
+// Copyright 2022 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Authors: Viviane Potocnik <vivianep@iis.ee.ethz.ch>
+
 #include <stdlib.h>
 
 __attribute__((weak, warning("64-bit arithmetic operation detected; ensure you're using the "
--- scripts/opentitan/generate_ot_ips.py
+++ scripts/opentitan/generate_ot_ips.py
@@ -1,3 +1,9 @@
+# Copyright 2022 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Authors: Viviane Potocnik <vivianep@iis.ee.ethz.ch>
+
 #!/usr/bin/env python3
 import os
 import subprocess
@@ -32,27 +38,28 @@ def main():
     # Compute absolute path to the OpenTitan repository.
     script_dir = os.path.dirname(__file__)
     opentitan_dir = os.path.abspath(
-        os.path.join(script_dir,
-                     "../../drivers/uart_opentitan/thirdparty/opentitan"))
+        os.path.join(script_dir, "../../drivers/uart_opentitan/thirdparty/opentitan")
+    )
 
     # Define paths to tools and key input files.
     regtool_py = os.path.join(opentitan_dir, "util/regtool.py")
     topgen_py = os.path.join(opentitan_dir, "util/topgen.py")
     dttool_py = os.path.join(opentitan_dir, "util/dttool.py")
     autogen_dif_py = os.path.join(opentitan_dir, "util/autogen_dif.py")
-    dt_ext_py = os.path.join(opentitan_dir,
-                             "hw/ip_templates/rstmgr/util/dt.py")
-    top_hjson = os.path.join(opentitan_dir,
-                             "hw/top_earlgrey/data/top_earlgrey.hjson")
+    dt_ext_py = os.path.join(opentitan_dir, "hw/ip_templates/rstmgr/util/dt.py")
+    top_hjson = os.path.join(opentitan_dir, "hw/top_earlgrey/data/top_earlgrey.hjson")
     autogen_dif_out = os.path.join(opentitan_dir, "sw/device/lib/dif/autogen")
 
     # Parse command-line arguments.
     parser = argparse.ArgumentParser(
-        description="Generate headers for embedded IPs for Chimera-SDK")
-    parser.add_argument("--output",
-                        type=str,
-                        required=True,
-                        help="Output directory for generated headers")
+        description="Generate headers for embedded IPs for Chimera-SDK"
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        required=True,
+        help="Output directory for generated headers",
+    )
     args = parser.parse_args()
     os.makedirs(args.output, exist_ok=True)
 
@@ -61,12 +68,42 @@ def main():
 
     # Define the set of IPs for which register headers should be generated.
     reg_ips = {
-        "uart", "adc_ctrl", "aes", "aon", "csrng", "alert_handler",
-        "aon_timer", "clkmgr", "edn", "entropy_src", "flash_ctrl", "gpio",
-        "hmac", "i2c", "keymgr", "keymgr_dpe", "kmac", "lc_ctrl", "otbn",
-        "otp_ctrl", "pattgen", "pinmux", "pwm", "pwrmgr", "rom_ctrl", "rstmgr",
-        "rv_core_ibex", "rv_plic", "rv_timer", "sensor_ctrl", "spi_device",
-        "spi_host", "sram_ctrl", "sysrst_ctrl", "usbdev", "rv_dm"
+        "uart",
+        "adc_ctrl",
+        "aes",
+        "aon",
+        "csrng",
+        "alert_handler",
+        "aon_timer",
+        "clkmgr",
+        "edn",
+        "entropy_src",
+        "flash_ctrl",
+        "gpio",
+        "hmac",
+        "i2c",
+        "keymgr",
+        "keymgr_dpe",
+        "kmac",
+        "lc_ctrl",
+        "otbn",
+        "otp_ctrl",
+        "pattgen",
+        "pinmux",
+        "pwm",
+        "pwrmgr",
+        "rom_ctrl",
+        "rstmgr",
+        "rv_core_ibex",
+        "rv_plic",
+        "rv_timer",
+        "sensor_ctrl",
+        "spi_device",
+        "spi_host",
+        "sram_ctrl",
+        "sysrst_ctrl",
+        "usbdev",
+        "rv_dm",
     }
 
     # Process each IP: run regtool, dttool, and autogen_dif.
@@ -79,26 +116,35 @@ def main():
     # Generate top-level autogen HJSON using topgen.
     cmd_topgen = [topgen_py, "-t", top_hjson, "-o", args.output]
     run_tool(cmd_topgen)
-    autogen_hjson = os.path.join(args.output,
-                                 "data/autogen/top_earlgrey.gen.hjson")
+    autogen_hjson = os.path.join(args.output, "data/autogen/top_earlgrey.gen.hjson")
 
     # Generate top-level DT files.
     cmd_dttool_top = [
-        dttool_py, "--topgencfg", autogen_hjson, "--outdir", args.output,
-        "--gen-top"
+        dttool_py,
+        "--topgencfg",
+        autogen_hjson,
+        "--outdir",
+        args.output,
+        "--gen-top",
     ]
     run_tool(cmd_dttool_top)
 
     # Generate IP-level DT headers.
     for ip_name, ip_hjson in ip_autogen_configs:
         cmd = [
-            dttool_py, "--topgencfg", autogen_hjson, "--outdir", args.output,
-            "--gen-ip", "-i", ip_hjson
+            dttool_py,
+            "--topgencfg",
+            autogen_hjson,
+            "--outdir",
+            args.output,
+            "--gen-ip",
+            "-i",
+            ip_hjson,
         ]
         if ip_name == "rstmgr":
             ipconfig_file = os.path.join(
                 opentitan_dir,
-                "hw/top_earlgrey/ip_autogen/rstmgr/data/top_earlgrey_rstmgr.ipconfig.hjson"
+                "hw/top_earlgrey/ip_autogen/rstmgr/data/top_earlgrey_rstmgr.ipconfig.hjson",
             )
             cmd.extend(["--ipconfig", ipconfig_file, "--extension", dt_ext_py])
         run_tool(cmd)
