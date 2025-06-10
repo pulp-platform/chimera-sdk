4928b59b5a38ded4f6e0dacbfba717d56e0cc54b
victor.jung <jungvi@iis.ee.ethz.ch>
2025-06-10 11:14:48 +0200

    Add proper licenses

--- generic_runtime/stdio.c
+++ generic_runtime/stdio.c
@@ -1,10 +1,8 @@
-/*
- * Copyright 2025 ETH Zurich.
- * Licensed under the Apache License, Version 2.0, see LICENSE for details.
- * SPDX-License-Identifier: Apache-2.0
- * 
- * Victor Jung <jungvi@iis.ee.ethz.ch>
- */
+// Copyright 2024 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Victor Jung <jungvi@iis.ee.ethz.ch>
 
 #include <stdio.h>
 
