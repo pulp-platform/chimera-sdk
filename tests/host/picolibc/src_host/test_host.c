c521dbc1c22a155aed2ef3c27d133410d2e8e17f
Philip Wiese <wiesep@iis.ee.ethz.ch>
2026-03-20 14:13:11 +0100

    Revert removing picolibc test

---
+++ tests/host/picolibc/src_host/test_host.c
@@ -0,0 +1,37 @@
+// SPDX-FileCopyrightText: 2025 ETH Zurich and University of Bologna
+// SPDX-License-Identifier: Apache-2.0
+
+// Include Standard Libraries
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdint.h>
+
+// Include Application Headers
+
+// Include Target Specific Headers
+
+// Include Driver Headers
+
+// Include Runtime Headers
+
+// Import HAL Headers
+
+int main(void) {
+    // memcpy
+    volatile int32_t src[4] = {1, 2, 3, 4}; // Use volatile to prevent optimization
+    volatile int32_t dst[4] = {0, 0, 0, 0};
+    memcpy((void *)dst, (const void *)src, sizeof(src));
+
+    // Math (Integer)
+    volatile int32_t intval = 42;   // Use volatile to prevent optimization
+    intval = __builtin_clz(intval); // Use volatile to prevent optimization
+    intval = __builtin_ctz(intval); // Use volatile to prevent optimization
+
+    // Math (Float)
+    volatile float testval = 0.5f;                           // Use volatile to prevent optimization
+    testval = sinf(testval) + cosf(testval) + tanf(testval); // Use volatile to prevent optimization
+    testval = logf(testval) + expf(testval);                 // Use volatile to prevent optimization
+
+    return 0;
+}
